e85937362cabc2ae0a97e684409f11600f7d7091
Abhiraj03 <rajabhirajusa@gmail.com>
2025-08-10 19:10:40 -0700

    Added About me Tech Stack Contact us etc

---
+++ data/about.ts
@@ -0,0 +1,36 @@
+import type { GalleryItem } from "@/app/components/about/AboutGallery";
+
+export const gallery: GalleryItem[] = [
+  {
+    src: "/images/abhiraj.jpg",
+    alt: "Teaching with a Quest in the VR lab",
+  },
+  {
+    src: "/images/abhiraj.jpg",
+    alt: "Hackathon weekend build",
+  },
+  {
+    src: "/images/abhiraj.jpg",
+    alt: "Long walk trails I love",
+  },
+  {
+    src: "/images/abhiraj.jpg",
+    alt: "Unreal Engine scene WIP",
+  },
+  {
+    src: "/images/abhiraj.jpg",
+    alt: "Tools I live in",
+  },
+  {
+    src: "/images/abhiraj.jpg",
+    alt: "Tempe sunset",
+  },
+  {
+    src: "/images/abhiraj.jpg",
+    alt: "Tempe sunset",
+  },
+  {
+    src: "/images/abhiraj.jpg",
+    alt: "Tempe sunset",
+  },
+];
